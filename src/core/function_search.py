5116d1524ff129ea13824bab760e3a33d4036f39
Christoph Kopp <christoph@kopp.me>
2026-04-09 13:58:30 +0200

    hamilton_folder used for Logic and MainFunction search

--- src/core/function_search.py
+++ src/core/function_search.py
@@ -2,15 +2,17 @@ import os
 import sys
 import glob
 
+import src.config as config
+
 def getLogicFunctions():
-    logicFiles = glob.glob('C:/Program Files (x86)/Hamilton/Library/**/*.smt', recursive=True)
+    logicFiles = glob.glob(f'{config.get("hamilton_folder")}/Library/**/*.smt', recursive=True)
     logicFunctions = [os.path.basename(x).split('.')[0].upper() for x in logicFiles if "LogicLayer" in x]
 
     return logicFunctions
 
 
 def getMainFunctions():
-    mainFiles = glob.glob('C:/Program Files (x86)/Hamilton/Methods/**/*.med', recursive=True)
+    mainFiles = glob.glob(f'{config.get("hamilton_folder")}/Methods/**/*.med', recursive=True)
     mainFunctions = [os.path.basename(x).split('.')[0].upper() for x in mainFiles]
 
     return mainFunctions
