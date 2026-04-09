8b82cd81153c31faa28ad964f1ebb77f8f74fede
Christoph Kopp <christoph@kopp.me>
2026-04-09 15:07:16 +0200

    added qr generator

--- src/qr_generator.py
+++ src/qr_generator.py
@@ -1,4 +1,51 @@
 import os
+import qrcode
+import datetime
 
-def create_qr_code(logger):
-    return
+import src.config as config
+
+from PIL import Image
+
+OUTPUT_FOLDER = os.path.join(config.get("output_folder"), "QRCodes")
+os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+
+def create_qr_code(path, logger):
+
+    if not path.strip():
+        logger.warning("No input provided")
+        return
+
+    pathLogo = os.path.join(config.get("image_folder"), "CS_quadratic.png")
+    logo = Image.open(pathLogo)
+
+    baseWidth= 100
+
+    wpercent = (baseWidth / float(logo.size[0]))
+    hsize = int((float(logo.size[1])*float(wpercent)))
+    logo = logo.resize((baseWidth, hsize))
+
+    QRcode = qrcode.QRCode(
+        error_correction=qrcode.constants.ERROR_CORRECT_H
+    )
+
+    QRcode.add_data(path)
+    QRcode.make()
+    QRcolor = (28, 45, 87)
+
+    QRimg = QRcode.make_image(
+        fill_color=QRcolor,
+        back_color="white"
+    ).convert("RGB")
+
+    pos = (
+        (QRimg.size[0] - logo.size[0]) // 2,
+        (QRimg.size[1] - logo.size[1]) // 2
+    )
+
+    QRimg.paste(logo, pos)
+
+    outputFile = os.path.join(OUTPUT_FOLDER, f"ISD_QR_{datetime.datetime.now().strftime('%Y%m%d-%H%M')}.png")
+
+    QRimg.save(outputFile)
+
+    logger.info(f"QR Code saved as {outputFile}")
--- ui/pages/qr_generator_page.py
+++ ui/pages/qr_generator_page.py
@@ -1,17 +1,34 @@
 import os
 
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLabel
+from src.workers import ScriptWorker
+from src.qr_generator import create_qr_code
+
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit
 
 class QRGeneratorPage(QWidget):
     def __init__(self, logger):
         super().__init__()
-
+        self.logger = logger
         layout = QVBoxLayout(self)
+        title = QLabel("QR Code Generator")
+        title.setObjectName("title")
+        layout.addWidget(title)
 
-        hint = QLabel("Note: This will come in Future Releases")
-        layout.addWidget(hint)
+        layout.addWidget(QLabel("Link"))
+        self.path_input = QLineEdit()
+        self.path_input.setPlaceholderText("www.hamiltoncompany.com")
+        layout.addWidget(self.path_input)
 
-        return
+        self.run_button = QPushButton("Generate QR Code")
+        self.run_button.clicked.connect(self._run_qr)
 
-        run_button = QPushButton("Generate QR Code")
-        layout.addWidget(run_button)
+        layout.addStretch()
+        layout.addWidget(self.run_button)
+
+
+    def _run_qr(self):
+        path = self.path_input.text()
+        self.logger.info("Generate QR Code button pressed")
+
+        self.worker = ScriptWorker(create_qr_code, (path, self.logger))
+        self.worker.start()
